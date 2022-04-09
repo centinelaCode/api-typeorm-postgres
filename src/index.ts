e7a8dba5e55c085b4aa178ece9566e4dd8078480
centinela1024tb <centinela1024tb@gmail.com>
2022-04-09 13:55:29 -0500

    [CONFIG] Configuraciń basica de express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello 2');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log('Server listen on port 3000')
+});
